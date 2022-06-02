23ba56db2331a027b1f122f025eca703af1390b4
Aloekior <purpledesert@posteo.nz>
2022-06-02 08:49:43 +0200

    Version 2 — no suitcase swap on last round, final map skipped due to while problem with bankoffer.

--- program/logic/DondRuntime.java
+++ program/logic/DondRuntime.java
@@ -8,66 +8,43 @@ public class DondRuntime {
     private static final int[] rounds = {6,5,4,3,2,1,1,1,1};
     private static int userCase;
     private static Scanner scanner = new Scanner(System.in);
+    private static double bankOffer = 0;
 
     public DondRuntime() {
         RunGame();
     }
 
     private static void RunGame() {
+        boolean bankDeal = false;
         createSuitcases();
         userCase = userSelection();
         playerValue = removeCase(userCase);
         printRemainingCases();
 
-        for (int i = 0; i < rounds.length; i++) {
-            while (rounds[i] > 0) {
+        for (int i = 1; i < rounds.length; i++) {
+            while (rounds[i - 1] > 0) {
                 int caseToRemove = userSelection();
                 System.out.println("Case [" + caseToRemove + "] contained: $" + removeCase(caseToRemove));
-                rounds[i]--;
+                rounds[i - 1]--;
+                printRemainingCases();
             }
-            double bankOffer = getBankOffer(i + 1);
+            double bankOffer = getBankOffer(i);
 
             System.out.println("The bank offers $" + bankOffer + " for your case — DEAL OR NO DEAL? (Enter 'DEAL' or press any button)");
             if (scanner.nextLine().equalsIgnoreCase("deal")) {
-                System.out.println("Congratulations, you accepted the banks offer of $" + bankOffer);
-                System.out.println("Your selected case [" + userCase + "] contained $" + playerValue);
-                System.out.println("Thank you for playing DEAL OR NO DEAL!");
+                bankDeal = true;
                 break;
             }
+            printRemainingCases();
         }
+        finale(bankDeal);
     }
 
     private static void createSuitcases() {
         boolean debug = debugSetup();
 
-        Double[] values = {
-                0.01,
-                1d,
-                5d,
-                10d,
-                25d,
-                50d,
-                75d,
-                100d,
-                200d,
-                300d,
-                400d,
-                500d,
-                750d,
-                1000d,
-                5000d,
-                10000d,
-                25000d,
-                50000d,
-                75000d,
-                100000d,
-                200000d,
-                300000d,
-                400000d,
-                500000d,
-                750000d,
-                1000000d
-        };
+        Double[] values = {0.01, 1d, 5d, 10d, 25d, 50d, 75d, 100d, 200d, 300d, 400d, 500d, 750d, 1000d, 5000d,
+                10000d, 25000d, 50000d, 75000d, 100000d, 200000d, 300000d, 400000d, 500000d, 750000d, 1000000d};
 
         if (!debug) {
             List<Double> shuffled = new ArrayList<>(Arrays.stream(values).toList());
@@ -79,12 +56,9 @@ public class DondRuntime {
             suitcases.put(i + 1, values[i]);
         }
         printRemainingCases();
-
     }
 
     private static boolean debugSetup() {
-
-
         System.out.print("Enter D/d for debugging mode ");
         if (scanner.nextLine().equalsIgnoreCase("d")) {
             return true;
@@ -98,7 +72,6 @@ public class DondRuntime {
         for (Double v : suitcases.values()) {
             totalValue += v;
         }
-
         return Math.round(totalValue / suitcases.size() * rounds[currentRound] / 10);
     }
 
@@ -136,5 +109,13 @@ public class DondRuntime {
             System.out.print("[" + key + "]");
         }
         System.out.println();
+    }
+
+    private static void finale(boolean bankDeal) {
+        if (bankDeal) {
+            System.out.println("Congratulations, you accepted the banks offer of $" + bankOffer);
+        }
+        System.out.println("Your selected case [" + userCase + "] contained $" + playerValue);
+        System.out.println("Thank you for playing DEAL OR NO DEAL!");
     }
 }
